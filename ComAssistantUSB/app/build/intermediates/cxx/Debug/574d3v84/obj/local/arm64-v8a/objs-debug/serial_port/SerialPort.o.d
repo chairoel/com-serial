c6e8f38f862b50bf10d0480c8d4d358b84d91c8a
Chairoel <projectchairul@gmail.com>
2025-03-10 11:13:16 +0700

    init android apps

---
+++ ComAssistantUSB/app/build/intermediates/cxx/Debug/574d3v84/obj/local/arm64-v8a/objs-debug/serial_port/SerialPort.o.d
@@ -0,0 +1,4 @@
+/Users/chairulamri/Documents/Transjakarta/AndroidNative/serailCom/ComAssistantUSB/app/build/intermediates/cxx/Debug/574d3v84/obj/local/arm64-v8a/objs-debug/serial_port/SerialPort.o: \
+  /Users/chairulamri/Documents/Transjakarta/AndroidNative/serailCom/ComAssistantUSB/app/src/main/jni/SerialPort.c \
+  /Users/chairulamri/Documents/Transjakarta/AndroidNative/serailCom/ComAssistantUSB/app/src/main/jni/SerialPort.h
+/Users/chairulamri/Documents/Transjakarta/AndroidNative/serailCom/ComAssistantUSB/app/src/main/jni/SerialPort.h:
